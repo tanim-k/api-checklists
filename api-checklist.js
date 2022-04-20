b8147c768578fb5b7877a262eb5c041fa5557fe8
tanim-k <tanimkhan877@gmail.com>
2022-04-20 18:00:51 +0600

    home work list

---
+++ api-checklist.js
@@ -0,0 +1,13 @@
+/*  1: object with property(string, boolean, number, object, array, method-function that return a property value)
+    2: template string- minimum 3 property will be dynamic(nested object property, array second element, another property)
+    3:1: no parameter arrow function here return 89
+    3:2: single parameter arrow function : will devide input by 7
+    3:3: 2 parameters arrow function : add 2 parameters : summation divided by 2
+    3:4: multiline arrow function: complex arrow function with two para meter : add them: then add 7 to the parameters then add them all, return summation
+    4: take an array. do map and devide each element by 7
+    5: map forEach FIlter find: difference among them
+    6: destructuring on object and array
+    7: jasonplaceholder theke photos data load kore console kore dekhao
+    8: mealDB er moto cocktailDB theke data loading and basic practice sportsDB practice
+    9: es6 fundamental concepts: == & === er difference 
+ */
